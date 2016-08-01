c979161e74c4958109f6f83aa728576c68a503c8
David Swearingen <swearingendw@mac.com>
2016-08-02 06:42:32 +0800

    tweaks

--- particle_timers/my-timer.ino
+++ particle_timers/my-timer.ino
@@ -1,9 +1,8 @@
 // Simple Timer (and serial) demo
 // From code sample on particle site
-// CONNECT THE DEVICE TO THE COMPUTER'S USB, THIS WILL ONLY WORK UNDER THOSE CIRCUMSTANCES
 
 // Then run:
-// particle serial monitor
+// particle serial monitor (serial monitor only works when particle is directly connected to pc via usb)
 
 // Watch the published messages on the command line:
 // curl https://api.particle.io/v1/devices/events?access_token=fa0d566664ad6e0f7b70f86f87c2b06e1d0a1170 (token may change)
