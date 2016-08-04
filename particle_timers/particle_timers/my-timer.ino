1f79ffe1aa61cfd51fdd14ca8718abf0923290ac
David Swearingen <swearingendw@mac.com>
2016-08-04 15:52:21 +0800

    re-organized directory

--- particle_timers/particle_timers/my-timer.ino
+++ particle_timers/particle_timers/my-timer.ino
@@ -9,10 +9,19 @@
 // Watch the published messages on the command line:
 // curl https://api.particle.io/v1/devices/events?access_token=fa0d566664ad6e0f7b70f86f87c2b06e1d0a1170 (token may change)
 
+// Note to self: if the IDE produces "Build didn't produce binary" error, then manually compile from the command line like so:
+// particle compile photon
+// particle flash <devicename> <photon_firmware_file_name.bin>
+
+// Circuit:
+// Place a CDS cell across pins A0 and A5 pins
+// Place a 12k ohm resistor across A0 and GND pins
+
 int analogvalue;
 int photoresistor = A0;
 int power = A5;
 
+// For debugging in the serial monitor
 void print_every_second()
 {
     static int count = 0;
@@ -21,7 +30,7 @@ void print_every_second()
 
 void read_light() {
     analogvalue = analogRead(photoresistor);
-    Particle.publish("light_value", String(analogvalue));
+    Particle.publish("light_value", String(analogvalue));  // Publish the data to the Particle Cloud
 }
 
 Timer timer1(3000, print_every_second);
